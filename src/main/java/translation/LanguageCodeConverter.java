fae59658a311161ce9289d5edfd9051559a15b31
storebe <storebe@icloud.com>
2025-09-23 12:27:58 -0400

    Andrew Task A: Passes all tests

--- src/main/java/translation/LanguageCodeConverter.java
+++ src/main/java/translation/LanguageCodeConverter.java
@@ -42,8 +42,15 @@ public class LanguageCodeConverter {
             iterator.next(); // skip the first line
             while (iterator.hasNext()) {
                 String line = iterator.next();
-                // TODO Task A: use line to populate the instance variables
-            }
+                String[] parts = line.trim().split("\\s+");
+                if (parts.length >= 2) {
+                    String code = parts[parts.length - 1].trim();
+                    String language = String.join(" ", java.util.Arrays.copyOf(parts, parts.length - 1)).trim();
+
+                    languageCodeToLanguage.put(code, language);
+                    languageToLanguageCode.put(language, code);
+                }
+                }
 
         } catch (IOException | URISyntaxException ex) {
             throw new RuntimeException(ex);
@@ -56,7 +63,7 @@ public class LanguageCodeConverter {
      * @return the name of the language corresponding to the code
      */
     public String fromLanguageCode(String code) {
-        // TODO Task A: update this code to use the correct instance variable to return the appropriate value
+        code = languageCodeToLanguage.get(code);
         return code;
     }
 
@@ -66,7 +73,7 @@ public class LanguageCodeConverter {
      * @return the 2-letter code of the language
      */
     public String fromLanguage(String language) {
-        // TODO Task A: update this code to use the correct instance variable to return the appropriate value
+        language = languageToLanguageCode.get(language);
         return language;
     }
 
