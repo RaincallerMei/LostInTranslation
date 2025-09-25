fa19fb282f3599e816cbf2905fa5ddfb0afe1711
harrymei <harrymei2006@gmail.com>
2025-09-25 19:48:46 -0400

    Changed all country codes in the hashmap to lowercase for consistency for Task B

--- src/main/java/translation/CountryCodeConverter.java
+++ src/main/java/translation/CountryCodeConverter.java
@@ -38,8 +38,8 @@ public class CountryCodeConverter {
             while (iterator.hasNext()) {
                 String line = iterator.next();
                 String[] parts = line.split("\t");
-                countryToCountryCode.put(parts[0], parts[2]);
-                countryCodeToCountry.put(parts[2], parts[0]);
+                countryToCountryCode.put(parts[0], parts[2].toLowerCase());
+                countryCodeToCountry.put(parts[2].toLowerCase(), parts[0]);
             }
         }
         catch (IOException | URISyntaxException ex) {
@@ -54,7 +54,7 @@ public class CountryCodeConverter {
      * @return the name of the country corresponding to the code
      */
     public String fromCountryCode(String code) {
-        return countryCodeToCountry.get(code.toUpperCase());
+        return countryCodeToCountry.get(code.toLowerCase());
     }
 
     /**
